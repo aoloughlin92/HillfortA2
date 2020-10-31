c4ed1e32d4a07357124523096991952e0078dedb
annemarie <20086619@mail.wit.ie>
2020-10-31 15:20:16 +0000

    A03 final

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -16,7 +16,6 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
   var hillfort = HillfortModel()
   lateinit var app : MainApp
-  //val hillforts = ArrayList<HillfortModel>()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -27,25 +26,30 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
     info("Hillfort Activity started..")
 
     app = application as MainApp
-
+    var edit = false
 
     if (intent.hasExtra("hillfort_edit")) {
+      edit = true
       hillfort = intent.extras?.getParcelable<HillfortModel>("hillfort_edit")!!
       hillfortTitle.setText(hillfort.title)
       description.setText(hillfort.description)
+      btnAdd.setText(R.string.save_hillfort)
     }
     
     btnAdd.setOnClickListener() {
       hillfort.title = hillfortTitle.text.toString()
       hillfort.description = description.text.toString()
-      if (hillfort.title.isNotEmpty()) {
-        app.hillforts.create(hillfort.copy())
-        info("add Button Pressed: ${hillfort}")
+      if (hillfort.title.isEmpty()) {
+        toast (R.string.enter_hillfort_title)
+      }else{
+        if(edit) {
+          app.hillforts.update(hillfort.copy())
+        }else{
+          app.hillforts.create(hillfort.copy())
+        }
+        info("add Button Pressed: $hillfortTitle")
         setResult(AppCompatActivity.RESULT_OK)
         finish()
-      }
-      else {
-        toast ("Please Enter a title")
       }
     }
 
