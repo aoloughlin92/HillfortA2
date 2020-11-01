04a8fe104322680717ebdc9f666b24930fc731da
annemarie <20086619@mail.wit.ie>
2020-11-01 14:29:03 +0000

    A06 Persistence

--- app/src/main/java/org/wit/hillfort/main/MainApp.kt
+++ app/src/main/java/org/wit/hillfort/main/MainApp.kt
@@ -3,19 +3,18 @@ package org.wit.hillfort.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.wit.hillfort.models.HillfortJSONStore
 import org.wit.hillfort.models.HillfortMemStore
 import org.wit.hillfort.models.HillfortModel
+import org.wit.hillfort.models.HillfortStore
 
 class MainApp : Application(), AnkoLogger {
 
-    lateinit var hillforts: HillfortMemStore
+    lateinit var hillforts: HillfortStore
 
     override fun onCreate() {
         super.onCreate()
-        hillforts = HillfortMemStore()
+        hillforts = HillfortJSONStore(applicationContext)
         info("Hillfort started")
-        //hillforts.add(HillfortModel("One", "About one..."))
-        //hillforts.add(HillfortModel("Two", "About two..."))
-        //hillforts.add(HillfortModel("Three", "About three..."))
     }
 }
